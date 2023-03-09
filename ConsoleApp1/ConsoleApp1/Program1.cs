574b57c42ed0799b7dff8535b7139cfb9d39ab30
Paul Schrum <paul.schrum@gmail.com>
2023-03-08 22:30:17 -0500

    Initial commit. In basic working order.

---
+++ ConsoleApp1/ConsoleApp1/Program1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Diagnostics;
+
+namespace CA1
+{
+    class Program1
+    {
+        static void Main()
+        {
+            Process pipeClient = new Process();
+
+            pipeClient.StartInfo.FileName = @"E:\SourceModules\CSharp\OtherMinor\LearningChannels" +
+                @"Rx\ConsoleApp1\ConsoleApp2\bin\Debug\net6.0\ConsoleApp2.exe";
+
+            using (NamedPipeServerStream pipeServer =
+                new NamedPipeServerStream("abc_named_pipe", PipeDirection.InOut))
+            {
+                Console.WriteLine("S: Named Pipe establihed.");
+
+                pipeClient.StartInfo.FileName = @"E:\SourceModules\CSharp\OtherMinor\LearningChannels" +
+                    @"Rx\ConsoleApp1\ConsoleApp2\bin\Debug\net6.0\ConsoleApp2.exe";
+
+                // Pass the client process a handle to the server.
+                pipeClient.StartInfo.UseShellExecute = false;
+                pipeClient.Start();
+
+                Console.WriteLine("S: Client started; Awaiting connection.");
+                pipeServer.WaitForConnection();
+                Console.WriteLine("S: Connected.");
+
+
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(pipeServer))
+                    {
+                        Thread.Sleep(500);
+                        sw.AutoFlush = true;
+                        Console.WriteLine("S: Enter text: ");
+                        sw.WriteLine(Console.ReadLine());
+                    }
+                }
+
+
+                // Catch the IOException that is raised if the pipe is broken
+                // or disconnected.
+                catch (IOException e)
+                {
+                    Console.WriteLine("[SERVER] Error: {0}", e.Message);
+                }
+            }
+
+            pipeClient.WaitForExit();
+            pipeClient.Close();
+            Console.WriteLine("[SERVER] Client quit. Server terminating.");
+        }
+    }
+}
+
+
+
+
---
+++ ConsoleApp1/ConsoleApp2/Program2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace CA2
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            using (NamedPipeClientStream pipeClient =
+                new NamedPipeClientStream(".", "abc_named_pipe", PipeDirection.InOut))
+            {
+
+                // Connect to the pipe or wait until the pipe is available.
+                Console.Write("     C: Attempting to connect to pipe...");
+                pipeClient.Connect();
+
+                Console.WriteLine("     C: Connected to pipe.");
+                Console.WriteLine($"     C: There are currently {pipeClient.NumberOfServerInstances}" +
+                    " pipe server instances open.");
+                using (StreamReader sr = new StreamReader(pipeClient))
+                {
+                    // Display the read text to the console
+                    string temp;
+                    while ((temp = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine("     C: Received from server: {0}", temp);
+                    }
+                }
+            }
+            Console.Write("     C: Press Enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
